02069a863515e3be497cf646fd32d09a52372d97
lucreziascaffidi <lucrezia.scaffidi@gmail.com>
2025-03-03 15:42:44 +0100

    feat: added show

--- src/main/java/com/example/spring_la_mia_pizzeria_crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/com/example/spring_la_mia_pizzeria_crud/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.spring_la_mia_pizzeria_crud.spring_la_mia_pizzeria_crud.model.Pizza;
@@ -24,4 +25,11 @@ public class PizzaController {
         model.addAttribute("pizzas", pizzas);
         return "pizzas/index";
     }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Integer id, Model model) {
+        model.addAttribute("pizza", repository.findById(id).get());
+        return "pizzas/show";
+    }
+
 }
